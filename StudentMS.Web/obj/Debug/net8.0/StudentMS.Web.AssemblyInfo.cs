bf9e84c44cca083bb87030305480b2301e068507
annshaphana <sujensujenthini@gmail.com>
2025-09-27 16:28:48 +0530

    web

--- StudentMS.Web/obj/Debug/net8.0/StudentMS.Web.AssemblyInfo.cs
+++ StudentMS.Web/obj/Debug/net8.0/StudentMS.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentMS.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18e3c157f591837f65aa25ef015c5215fe472d47")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentMS.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentMS.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
